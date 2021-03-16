7af933a3864576d5e459df64d25f0bfda52b9bd8
abakarovmaks <abakarovmaks@gmail.com>
2021-03-16 16:25:27 +0200

    Изменяет разметку, меняет фотографии

--- src/js/siema.js
+++ src/js/siema.js
@@ -2,7 +2,7 @@ import Siema from 'siema';
 
 const mySiema = new Siema({
   selector: '.siema',
-  duration: 500,
+  duration: 1500,
   easing: 'cubic-bezier(0.4, 0, 0.2, 1)',
   perPage: 1,
   startIndex: 0,
